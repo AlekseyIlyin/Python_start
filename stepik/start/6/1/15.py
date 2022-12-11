286b041cfb703ddffb2827e13bf5c295e44ff9db
Aleksey Ilin <kayrus@mail.ru>
2022-12-11 12:49:33 +0300

    6.1 tasks: 5 - 16

---
+++ stepik/start/6/1/15.py
@@ -0,0 +1,7 @@
+# Абсолютная сумма
+dig1 = float(input())
+dig2 = float(input())
+dig3 = float(input())
+dig4 = float(input())
+dig5 = float(input())
+print(abs(dig1) + abs(dig2) + abs(dig3) + abs(dig4) + abs(dig5))
